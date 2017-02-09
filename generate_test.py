1981696a533b64eead863bc4906e6dac155f0e7f
Yannik <me@adnidor.de>
2017-02-09 19:51:46 +0100

    made generate_test.py more verbose

--- generate_test.py
+++ generate_test.py
@@ -3,6 +3,9 @@ from random import randrange
 import vc820
 import sys
 
+messages_to_create = 50
+counter = 0
+
 def _generate_bytes():  
     bts = bytes()
     for segment in range (1,15):
@@ -15,19 +18,25 @@ def get_random_list(count):
     lst = []
     for i in range(count):
         while True:
+            global counter
+            counter += 1
             bts = _generate_bytes()
+            mm = None
             try:
                 mm = vc820.MultimeterMessage(bts)
-                mm.value
             except (ValueError, AttributeError) as e:
                 continue
-            print("found valid message")
+            print("found valid message (%d): %s - %s"%(i,bts.hex(),str(mm)))
             lst.append(bts)
             break
     return lst
 
-filename = sys.argv[1]
-testvaluesfile = open(filename, "wb")
 
-for item in get_random_list(20):
-    testvaluesfile.write(item)
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    testvaluesfile = open(filename, "wb")
+
+    for item in get_random_list(messages_to_create):
+        testvaluesfile.write(item)
+
+    print("Tried %d Messages, found %d valid"%(counter, messages_to_create))
